a5ae6edd4cda4df55e6c586ea2a2db3643f42880
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-09-23 01:05:34 -0500

    UI Implemented

---
+++ app/src/main/java/com/unicauca/ubicuas/bleperipheral/MainActivity.java
@@ -0,0 +1,61 @@
+package com.unicauca.ubicuas.bleperipheral;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+
+    TextView txtTemp, txtState;
+    SeekBar temp;
+    Switch state;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtTemp = (TextView) findViewById(R.id.txt_temp);
+        txtState = (TextView) findViewById(R.id.txt_state);
+
+        state = (Switch) findViewById(R.id.state);
+        state.setOnClickListener(this);
+
+        temp = (SeekBar) findViewById(R.id.temp);
+        temp.setOnSeekBarChangeListener(this);
+
+        txtTemp.setText(""+ temp.getProgress()+"°");
+    }
+
+
+    //region Snackbar Listener
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        txtTemp.setText(""+progress+"°");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+    //endregion
+
+    //region Switch Listener
+    @Override
+    public void onClick(View v) {
+        if(state.isChecked()) {
+
+        }else {
+
+        }
+    }
+    //endregion
+}
